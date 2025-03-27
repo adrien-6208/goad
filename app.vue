8f10b4bbf0d56ade54e3ea99f6f4c304cd6fb9f5
Adrien <ago@monizze.be>
2025-03-27 16:48:31 +0100

    init pages folder, and layouts folder

--- app.vue
+++ app.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <div>
-      <div class="flex justify-center items-center min-h-screen bg-gray-100">
-        <h1 class="text-4xl md:text-6xl font-extrabold text-black-600 drop-shadow-md text-center">
-          👽🚀 Hello World 🚀👽
-        </h1>
-      </div>
-    </div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <div>
+        <div class="flex justify-center items-center">
+          <h1 class="text-4xl md:text-6xl font-extrabold text-black-600 drop-shadow-md text-center">
+            👽🚀 Hello World 🚀👽
+          </h1>
+        </div>
+      </div>
+    </div>
+  </template>

c1c419cc1e5623a63073ba82fcd4b7aaa8a7fc02
Adrien <ago@monizze.be>
2025-03-27 16:01:48 +0100

    add some text to see the build

--- app.vue
+++ app.vue
@@ -2,6 +2,10 @@
   <div>
     <div>
       <h1>Hello, world.</h1>
+
+      <h2>This is a test of cloudflare X Nuxt Hub.</h2>
+
+      <h2>New feature coming soon.</h2>
     </div>
   </div>
 </template>

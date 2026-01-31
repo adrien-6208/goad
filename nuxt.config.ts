e2deeea542f4788e97d2911025f7e258a39afc92
Adrien godaert <godaert.adrien@gmail.com>
2026-01-31 16:39:23 +0100

    remove pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,35 +14,10 @@ export default defineNuxtConfig({
     '@nuxt/test-utils',
     '@nuxt/ui',
     '@nuxthub/core',
-    '@vite-pwa/nuxt',
   ],
   vite: {
     plugins: [
       tailwindcss(),
     ],
-  },
-  pwa: {
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'One Piece Card Collection',
-      short_name: 'OP Cards',
-      description: 'Ma collection de cartes One Piece',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
-      display: 'standalone',
-      start_url: '/',
-      icons: [
-        {
-          src: '/images/icon-app.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/images/icon-app.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
   }
 })
